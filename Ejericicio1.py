c4f8fdb9c3377e8f2d3c934bc2e24f7dd9d13959
Sebas2 Pérez2 <Jspc513@gmail.com>
2024-07-29 16:54:21 -0500

    Agrego el ejercicio1 al repositorio

---
+++ Ejericicio1.py
@@ -0,0 +1,19 @@
+##DEFINICIÓN VARIABLES
+mensaje = 'Hola, estoy aprendiendo Python.' ; formacion = 'SENA'; nombre = 'Juan Sebastian Perez Cedano'
+edad = 16 ; estatura = 1.7256 ; peso = 60 
+
+#Forma de imprimir 1
+print(mensaje,'Mi nombre es:',nombre, 'Soy de la formacion:',formacion, 'tengo:',edad,'y mi estatura es de:',"%.3f" % estatura,'.Actualmente peso:',peso,'kg')
+print('\n')
+
+#Forma de imprimir 2
+print(f'{mensaje}mi nombre es: {nombre} soy de la formacion:{formacion}. Tengo {edad} años, mido {estatura} y tengo un peso de: {peso} kg')
+print('\n')
+
+##DEFINIR VARIABLES Y OPERARLAS
+a = 24
+b = 2
+print('-Multiplicacion=',a*b,'Division=',a//b,'Suma=',a+b,'Resta=',a-b,'Division Res=',a%b)
+print(f'-Multiplicacion={a*b},Division={a//b},Suma={a+b},Resta={a-b},Division,Res={a%b}')
+
+
