11c11a73ac4871fba67d5852db13df73fff6b860
Sebas2 Pérez2 <Jspc513@gmail.com>
2024-07-29 16:59:06 -0500

    Agrego la carpeta ejeercicios SENA al home

--- Ejericicio1.py
+++ Ejericicio1.py
@@ -3,17 +3,21 @@ mensaje = 'Hola, estoy aprendiendo Python.' ; formacion = 'SENA'; nombre = 'Juan
 edad = 16 ; estatura = 1.7256 ; peso = 60 
 
 #Forma de imprimir 1
-print(mensaje,'Mi nombre es:',nombre, 'Soy de la formacion:',formacion, 'tengo:',edad,'y mi estatura es de:',"%.3f" % estatura,'.Actualmente peso:',peso,'kg')
+print(mensaje,'Mi nombre es:',nombre, 'Soy de la formacion:',formacion, 'tengo:',edad,'y mi estatura es de:',"%.3f" % estatura,'Actualmente peso:',peso,'kg')
 print('\n')
 
 #Forma de imprimir 2
-print(f'{mensaje}mi nombre es: {nombre} soy de la formacion:{formacion}. Tengo {edad} años, mido {estatura} y tengo un peso de: {peso} kg')
+print(f'{mensaje}mi nombre es: {nombre} soy de la formacion:{formacion} .Tengo {edad} años, mido {estatura} y tengo un peso de: {peso} kg')
 print('\n')
 
 ##DEFINIR VARIABLES Y OPERARLAS
-a = 24
-b = 2
+a = int(input('Ingresa un digito: '))
+b = int(input('Ingresa un digito: '))
+
+
 print('-Multiplicacion=',a*b,'Division=',a//b,'Suma=',a+b,'Resta=',a-b,'Division Res=',a%b)
 print(f'-Multiplicacion={a*b},Division={a//b},Suma={a+b},Resta={a-b},Division,Res={a%b}')
+
+
 
 
