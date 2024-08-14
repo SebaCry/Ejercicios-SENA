267e37c73e916c375470ebff394f2c545b414833
Sebas2 Pérez2 <Jspc513@gmail.com>
2024-08-14 09:30:44 -0500

    Elimino un archivo, que era posible solucion al problema 8, pero se hizo de otra manera con el uso de rangos y posiciones

--- EstructurasSelectivas/ejercicio8Reremasterizado.py
+++ EstructurasSelectivas/ejercicio8Reremasterizado.py
@@ -1,30 +1,40 @@
 print('¿Tienes Anemia?\n')  # Imprimo el nombre del programa
 
-# Pregunto al usuario si es masculino o femenino
-sexo = str(input('¿Cual es tu sexo?: ')).lower().strip()
+edadTipo = str(input('¿Escoje el tipo de edad (bebe, niño o adulto): ')).lower().strip()
+hemoglobina = float(input('¿Cual es tu rango de hemoglobina actual?🤨: '))
 
-# Pregunto al usuario su edad y género
-edad = int(input('¿Cuantos años tienes?: '))
-
-# Defino el rango de hemoglobina que corresponde a cada edad y sexo
-if sexo == 'femenino':
-    if edad <= 15:
-        rango_hemoglobina = (12, 16)
+if edadTipo == 'bebe':
+    edadMeses = int(input('¿Cuantos meses tienes?: '))
+    if edadMeses == 0 or edadMeses == 1:
+        rangoHemoglobina = (13,26)
+    elif edadMeses > 1 and edadMeses <= 6:
+        rangoHemoglobina = (10,18)
+    elif edadMeses > 6 and edadMeses <= 12:
+        rangoHemoglobina = (11,15)
     else:
-        rango_hemoglobina = (11.5, 15)
-elif sexo == 'masculino':
-    if edad <= 15:                                ##ESTRUCTURA CONDICIONAL PARA MAÑANA, ACUERDATE HACERLO ASI, PERO CON E S A
-        rango_hemoglobina = (14, 18)
+        print('Escribe un dígito coherente')
+elif edadTipo == 'niño':
+    edad = int(input('¿Cuantos años tienes?: '))
+    if edad >= 1 and edad <= 5:
+        rangoHemoglobina = (11.5,15)
+    elif edad > 5 and edad <= 10:
+        rangoHemoglobina = (12.6,15.5)
+    elif edad > 10 and edad <= 15:
+        rangoHemoglobina = (13,15.5)
     else:
-        rango_hemoglobina = (13, 15.5)
+        print('Escribe un dígito coherente')
+elif edadTipo == 'adulto':
+    sexo = str(input('¿Cual es tu sexo?: ')).lower().strip()
+    if sexo == 'femenino':
+        rangoHemoglobina = (12,16)
+    elif sexo == 'masculino':
+        rangoHemoglobina = (14,18)
+    else:
+        print('Escribe un dígito coherente')
 else:
-    print('Escribe un digito coherente')
-    exit()
+    print('Escribe un dígito coherente')
 
-# Comparo el nivel de hemoglobina ingresado con el rango correspondiente
-if rango_hemoglobina[0] <= hemoglobina <= rango_hemoglobina[1]:
-    print('Negativo para hemoglobina 😉')
-else:
+if hemoglobina < rangoHemoglobina[0] or hemoglobina > rangoHemoglobina[1]:
     print('Positivo para anemia, lo sentimos 😥')
-
-    aksjaskjas
+else:
+    print('Negativo para hemoglobina 😉')
